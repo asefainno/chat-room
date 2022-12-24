9572748283e9187188f95c9de6bad921c27456b3
asefaInno <asefa.inno@gmail.com>
2022-12-24 13:59:33 +0700

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const firebaseConfig = {
+    apiKey: "AIzaSyCofpGDhWncXT0dPgUO1U-mgkm81xrf6_U",
+    authDomain: "asefachat.firebaseapp.com",
+    databaseURL: "https://asefachat-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "asefachat",
+    storageBucket: "asefachat.appspot.com",
+    messagingSenderId: "22523063784",
+    appId: "1:22523063784:web:68ab7dc9698ef31866085d",
+    measurementId: "G-EXYBBTH66Z",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  // const analytics = getAnalytics(app);
+  console.log("[app]", app);
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
